a5461a6ed7ac62beeb4bad0e25643492cb56e45c
charlesren <renccn@gmail.com>
2019-08-09 20:15:45 +0800

    user filepath.Walk methord

--- fileNumCheck.go
+++ fileNumCheck.go
@@ -1,38 +1,31 @@
 package main
 
 import (
-	"bytes"
 	"fmt"
-	"os/exec"
-
-	"github.com/bitfield/script"
+	"io/ioutil"
+	"os"
+	"path/filepath"
 )
 
+// ThordHold is a level
+var ThordHold = 2000
+
 func main() {
-	var ThordHold int
-	ThordHold = 20000
-	var Dir = []string{"/"}
-	for a := &Dir; ; {
-		if len(*a) == 0 {
-			fmt.Println("All directory had been checked!!!")
-			break
-		} else {
-			fmt.Println("hello")
-			q := script.NewPipe()
-			cmd := exec.Command("ls", "-al", (*a)[0])
-			output, err := cmd.CombinedOutput()
-			if err != nil {
-				q.SetError(err)
-			}
-			q.WithReader(bytes.NewReader(output))
-			var totalnum int
-			totalnum, err = q.CountLines()
-			if totalnum < ThordHold {
-				fmt.Printf("There are %v file or directory in %v !!!\n", totalnum, (*a)[0])
-			}
-			data := string(output)
-			fmt.Println(data)
-			*a = (*a)[1:]
+	filepath.Walk("/", checkNum)
+
+}
+func checkNum(path string, f os.FileInfo, err error) error {
+	if f == nil {
+		return err
+	}
+	if f.IsDir() {
+		dir, err := ioutil.ReadDir(path)
+		if err != nil {
+			return err
+		}
+		if len(dir) < ThordHold {
+			fmt.Printf("There are %v file or directory in %v !!!\n", len(dir), path)
 		}
 	}
+	return err
 }
