bf8f5704ef241fbe260791456bb42831c3392436
charlesren <renccn@gmail.com>
2019-08-17 14:35:56 +0800

    add GetStCode func

--- utils.go
+++ utils.go
@@ -8,7 +8,14 @@ import (
 // SaveData save check result
 func SaveData() string {
 	cfgItem := "abc#1,25;1,30;2,35;4,40#5"
-	checkHold := strings.Split(cfgItem, "#")[1]
-	fmt.Println(checkHold)
-	return checkHold
+	thordHolds := strings.Split(cfgItem, "#")[1]
+	fmt.Println(thordHolds)
+	return thordHolds
+}
+
+// GetStCode return status code
+func GetStCode(data float64, cfgItem string) int {
+	thordHolds := strings.Split(cfgItem, "#")[1]
+	fmt.Println(thordHolds)
+	return 1
 }
