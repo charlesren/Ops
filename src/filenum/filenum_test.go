f4cafdd9713327033c479586caa54fe73973a3d4
charlesren <renccn@gmail.com>
2019-08-18 09:36:13 +0800

    delete unused fun

--- src/filenum/filenum_test.go
+++ src/filenum/filenum_test.go
@@ -7,13 +7,6 @@ import (
 	"testing"
 )
 
-func main() {
-	WalkDir := "/usr"
-	ThordHold = 1000
-	filepath.Walk(WalkDir, CheckNum)
-	fmt.Println(Files)
-	entegor.SaveData()
-}
 func TestCheckNum(t *testing.T) {
 	WalkDir := "/usr"
 	ThordHold = 1000
