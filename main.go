f893a47bed28eb29f73d984968d2bec006168618
charlesren <renccn@gmail.com>
2019-08-27 20:57:44 +0800

    change result

--- main.go
+++ main.go
@@ -25,12 +25,10 @@ func main() {
 	INIFile := "./inifile.ini"
 	HostIP12 := "011111111111"
 	HostIP := "11.111.111.111"
-	fmt.Println(INIFile, HostIP12, HostIP)
 	WorkDir, err := os.Getwd()
 	if err != nil {
 		panic(err)
 	}
-	fmt.Println(WorkDir)
 	_, fullScriptName := filepath.Split(os.Args[0])
 	scriptName := strings.Split(fullScriptName, ".")[0]
 	TmpDir := filepath.Join(WorkDir, "temp")
@@ -55,7 +53,6 @@ func main() {
 	}
 	fmt.Println(hostname)
 	now := time.Now().Format(entegor.LongForm)
-	fmt.Println(now)
 	ini, err := os.Open(INIFile)
 	if err != nil {
 		log.Fatalf("failed to open file: %s", err)
@@ -63,9 +60,7 @@ func main() {
 	iniScanner := bufio.NewScanner(ini)
 	for iniScanner.Scan() {
 		cfgItem := iniScanner.Text()
-		fmt.Println("cfgItem:", cfgItem)
 		walkDir := strings.Split(cfgItem, "|")[0]
-		fmt.Println(walkDir)
 		filenum.ThordHold, _ = strconv.Atoi(strings.Split(strings.Split(cfgItem, "#")[0], "|")[1])
 		fmt.Println(filenum.ThordHold)
 		filepath.Walk(walkDir, filenum.CheckNum)
@@ -77,24 +72,27 @@ func main() {
 			Data = float64(1)
 		}
 		stCode := entegor.GetStCode(Data, cfgItem)
-		fmt.Println(stCode)
-		var result string
 		descMsg := walkDir
 		good := entegor.GetGood(cfgItem)
 		stCodeString := strconv.Itoa(stCode)
 		DataString := strconv.FormatFloat(Data, 'E', 1, 64)
 		warnMsg := ""
+		var result string
 		if stCode == 0 {
 			head := entegor.GetHead(cfgItem)
-			result = head + "=" + stCodeString + "|" + now + "|" + DataString + "|" + good + "|" + descMsg
+			result = head + "=" + stCodeString + "|" + now + "|" + DataString + "|" + good + "|" + descMsg + "\n"
 		} else {
 			head := entegor.GetWarningHead(cfgItem)
-			result = head + "=" + stCodeString + "|" + now + "|" + "AOMS" + "|" + "fullScriptName" + "|" + "filenum.ErrCode" + "|" + hostname + "|" + HostIP + "|" + "" + "|" + "" + "|" + warnMsg
+			result = head + "=" + stCodeString + "|" + now + "|" + "AOMS" + "|" + fullScriptName + "|" + filenum.ErrCode + "|" + hostname + "|" + HostIP + "|" + "" + "|" + "" + "|" + warnMsg + "\n"
 		}
+		sysutil.AppendToFile(OutFile, result)
+		fmt.Println(filenum.Files)
 		for _, file := range filenum.Files {
+			fmt.Println(file)
 			sysutil.AppendToFile(OutTmpFile, file.Name)
 		}
-		sysutil.AppendToFile(OutTmpFile, result)
+		sysutil.AppendToFile(OutFile, result)
+		sysutil.WriteToFile(OutTmpFile, result)
 		result = entegor.SaveData(stCode, cfgItem, now, Data, walkDir)
 		fmt.Println(result)
 	}
